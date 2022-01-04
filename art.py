7171cb695e3a23ff0ef43ff907bf77113004621b
Sivaprasad.G <sivaprasad.g@live.com>
2022-01-04 16:41:34 +0530

    caesar_cypher final

--- art.py
+++ art.py
@@ -1,12 +1,11 @@
-<pre>
-      
+logo = """           
  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
 a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
 8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
 "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
  `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
-                          88                                 
-                          88                                 
+            88             88                                 
+           ""             88                                 
                           88                                 
  ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
 a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
@@ -15,7 +14,4 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
  `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
               88                                             
               88           
-
-</pre>
-
-A Caesar cipher is a simple method of encoding messages. Caesar ciphers use a substitution method where letters in the alphabet are shifted by some fixed number of spaces to yield an encoding alphabet.
+"""
